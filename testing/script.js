4081a1c4d556b1b1c5f3e65ab4bf15c8b2583a8e
1sihamixa <92829536+1sihamixa@users.noreply.github.com>
2024-06-12 15:26:39 +0100

    Add files via upload

---
+++ testing/script.js
@@ -0,0 +1,27 @@
+// function myFunction(x) {
+//     if (x % 2 === 0) {
+//         return "Even";
+//     }else{
+//         return "Odd"
+//     };
+// };
+// document.getElementById("demo").innerHTML = myFunction(13);
+
+// **************************************
+
+// let strOne = "siham";
+// let strTwo = "aya";
+
+// document.getElementById("emo").innerHTML = `${strOne} are ${strTwo}`;
+
+// ******************************************
+
+// let str = "abcdzf";
+// str = str.substring(0,4) + "e" + str.substring(5);
+// document.getElementById("de").innerHTML = str;
+
+// *********************************************
+
+
+
+
